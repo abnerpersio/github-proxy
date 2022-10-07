2bbaa0736c68f2cdc4090d77c0a56b0f49448fdd
Abner Persio <binhopersio@gmail.com>
2022-10-07 05:25:25 -0300

    feat: implements github service

---
+++ src/services/github-service.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+import { GithubUser, Repo, UserDetail } from '../infra/types/github';
+import { BASE_GITHUB_URL, endpoints } from '../shared/constants/github';
+
+export class GithubService {
+  async listUsers(since?: number): Promise<{ users: GithubUser[]; nextPageLink: string }> {
+    const url = this.generateUrl(endpoints.users);
+
+    const response = await axios.get<GithubUser[]>(url, {
+      params: { since },
+    });
+
+    const users = response?.data ?? [];
+    const nextPageLink = response.headers.link ?? '';
+
+    return { users, nextPageLink };
+  }
+
+  async getUser(username: string): Promise<UserDetail> {
+    const url = this.generateUrl(`${endpoints.users}/${username}`);
+    return (await axios.get(url))?.data;
+  }
+
+  async listRepos(username: string): Promise<Repo[]> {
+    const url = this.generateUrl(`${endpoints.users}/${username}/repos`);
+    return (await axios.get(url))?.data;
+  }
+
+  private generateUrl(endpoint: string) {
+    return `${BASE_GITHUB_URL}/${endpoint}`;
+  }
+}
